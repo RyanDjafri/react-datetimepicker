f9952985f8455ab9048c6cf7799267607d7eea97
RyanDjafri <ryandjafripro@gmail.com>
2024-06-18 13:06:31 +0200

    adds

--- src/DateTimePicker.js
+++ src/DateTimePicker.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import moment from "moment";
 import "./DateTimePicker.css";
-
-moment.locale("en"); // Set locale to U.S. English
+moment.locale("en");
 
 const DateTimePicker = ({ onChange, value }) => {
   const [selectedDate, setSelectedDate] = useState(
@@ -11,6 +10,27 @@ const DateTimePicker = ({ onChange, value }) => {
 
   const handleDateChange = (event) => {
     const date = moment(event.target.value, "YYYY-MM-DD");
+    if (date.isValid()) {
+      setSelectedDate((prevDate) => {
+        const newDate = moment(prevDate).set({
+          year: date.year(),
+          month: date.month(),
+          date: date.date(),
+        });
+        if (onChange) {
+          onChange(newDate);
+        }
+        return newDate;
+      });
+    }
+  };
+
+  const handleTimeChange = (event) => {
+    const time = event.target.value.split(":");
+    const date = moment(selectedDate).set({
+      hour: time[0],
+      minute: time[1],
+    });
     if (date.isValid()) {
       setSelectedDate(date);
       if (onChange) {
@@ -26,6 +46,11 @@ const DateTimePicker = ({ onChange, value }) => {
           type="date"
           value={selectedDate.format("YYYY-MM-DD")}
           onChange={handleDateChange}
+        />
+        <input
+          type="time"
+          value={selectedDate.format("HH:mm")}
+          onChange={handleTimeChange}
         />
       </div>
     </div>
